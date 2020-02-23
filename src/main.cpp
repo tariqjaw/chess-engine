34d322d6e02f63f9eacb99e371c16ad8f22ab4f6
Tariq Ameer <60607660+tariqjaw@users.noreply.github.com>
2020-02-23 22:56:08 +0000

    testing new functions

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,30 @@
 #include <iostream>
 #include "board.hpp"
 #include "types.hpp"
+#include "hash.hpp"
+#include <time.h>
 
 int main() {
 	Board my_board;
+	HashKey h_key;
 	for (int i = 0; i < 64; i++) {
 		my_board.ShowBoard(my_board.GetPiece(i));
 	};
-
+	my_board.ShowBoard(my_board.KingMoves(my_board.GetWhiteKing(), my_board.GetMaskRank(7)));
+	my_board.ShowBoard(my_board.KnightMoves(my_board.GetWhiteKnights(), my_board.GetMaskRank(7)));
+	my_board.ShowBoard(my_board.WhitePawnMoves(my_board.GetWhitePawns(), my_board.GetBlackPieces(), my_board.GetAllPieces()));
+	my_board.ShowBoard(my_board.BlackPawnMoves(my_board.GetBlackPawns(), my_board.GetWhitePieces(), my_board.GetAllPieces()));
+	my_board.ParseFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", my_board);
+	my_board.ShowBoard(my_board.GetClearFile(FILE_A));
+	my_board.ShowBoard(my_board.GetWhiteKnights());
+	std::cout << "Count: " << my_board.CountBits(my_board.GetWhiteKnights()) << "\n";
+	U64 temp = my_board.GetWhiteKnights();
+	std::cout << "Index: " << my_board.PopBit(&temp) << "\n";
+	my_board.SetBit(temp, G5);
+	my_board.ShowBoard(temp);
+	my_board.ClearBit(temp, G1);
+	my_board.ShowBoard(temp);
+	srand(time(NULL));
+	std::cout << h_key.Rand64();
 	return 0;
 };
