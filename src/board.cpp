d0ba9b8fa7a18e0d10f5dc9dc8b267a73f42a710
Tariq Ameer <60607660+tariqjaw@users.noreply.github.com>
2020-02-25 12:39:36 +0000

    Update board.cpp

--- src/board.cpp
+++ src/board.cpp
@@ -7,8 +7,8 @@
 
 Board::Board() {
 	//precomputed lookup tables
-  clear_rank_[RANK_1] = 0xFFFFFFFFFFFFFF00;
-  clear_rank_[RANK_2] = 0xFFFFFFFFFFFF00FF;
+  clear_rank_[RANK_1] = 0xFFFFFFFFFFFFFF00; // every bit on the board is set to 1 except the rank at the current index
+  clear_rank_[RANK_2] = 0xFFFFFFFFFFFF00FF; // this allows us to compute without considering a certain rank
   clear_rank_[RANK_3] = 0xFFFFFFFFFF00FFFF;
   clear_rank_[RANK_4] = 0xFFFFFFFF00FFFFFF;
   clear_rank_[RANK_5] = 0xFFFFFF00FFFFFFFF;
@@ -16,32 +16,32 @@ Board::Board() {
   clear_rank_[RANK_7] = 0xFF00FFFFFFFFFFFF;
   clear_rank_[RANK_8] = 0x00FFFFFFFFFFFFFF;
 
-  mask_rank_[FILE_A] = 0x00000000000000FF;
-  mask_rank_[FILE_B] = 0x000000000000FF00;
-  mask_rank_[FILE_C] = 0x0000000000FF0000;
-  mask_rank_[FILE_D] = 0x00000000FF000000;
-  mask_rank_[FILE_E] = 0x000000FF00000000;
-  mask_rank_[FILE_F] = 0x0000FF0000000000;
-  mask_rank_[FILE_G] = 0x00FF000000000000;
-  mask_rank_[FILE_H] = 0xFF00000000000000;
+	clear_file_[FILE_A] = 0xFEFEFEFEFEFEFEFE; // same concept as clearing a rank, just vertical
+	clear_file_[FILE_B] = 0xFDFDFDFDFDFDFDFD; // this is especially useful for preventing pieces from travelling off the board
+	clear_file_[FILE_C] = 0xFBFBFBFBFBFBFBFB;
+	clear_file_[FILE_D] = 0xF7F7F7F7F7F7F7F7;
+	clear_file_[FILE_E] = 0xEFEFEFEFEFEFEFEF;
+	clear_file_[FILE_F] = 0xDFDFDFDFDFDFDFDF;
+	clear_file_[FILE_G] = 0xBFBFBFBFBFBFBFBF;
+	clear_file_[FILE_H] = 0x7F7F7F7F7F7F7F7F;
 
-	clear_file_[0] = 0xFEFEFEFEFEFEFEFE;
-	clear_file_[1] = 0xFDFDFDFDFDFDFDFD;
-	clear_file_[2] = 0xFBFBFBFBFBFBFBFB;
-	clear_file_[3] = 0xF7F7F7F7F7F7F7F7;
-	clear_file_[4] = 0xEFEFEFEFEFEFEFEF;
-	clear_file_[5] = 0xDFDFDFDFDFDFDFDF;
-	clear_file_[6] = 0xBFBFBFBFBFBFBFBF;
-	clear_file_[7] = 0x7F7F7F7F7F7F7F7F;
+  mask_rank_[RANK_1] = 0x00000000000000FF; // bits are turned on for the rank at the current index
+  mask_rank_[RANK_2] = 0x000000000000FF00; // useful for only computing pieces on a specific rank, such as a double-stepping pawn
+  mask_rank_[RANK_3] = 0x0000000000FF0000;
+  mask_rank_[RANK_4] = 0x00000000FF000000;
+  mask_rank_[RANK_5] = 0x000000FF00000000;
+  mask_rank_[RANK_6] = 0x0000FF0000000000;
+  mask_rank_[RANK_7] = 0x00FF000000000000;
+  mask_rank_[RANK_8] = 0xFF00000000000000;
 
-	mask_file_[0] = 0x101010101010101;
-	mask_file_[1] = 0x202020202020202;
-	mask_file_[2] = 0x404040404040404;
-	mask_file_[3] = 0x808080808080808;
-	mask_file_[4] = 0x1010101010101010;
-	mask_file_[5] = 0x2020202020202020;
-	mask_file_[6] = 0x4040404040404040;
-	mask_file_[7] = 0x8080808080808080;
+	mask_file_[FILE_A] = 0x101010101010101; // same concept as masking a rank, but for files
+	mask_file_[FILE_B] = 0x202020202020202;
+	mask_file_[FILE_C] = 0x404040404040404;
+	mask_file_[FILE_D] = 0x808080808080808;
+	mask_file_[FILE_E] = 0x1010101010101010;
+	mask_file_[FILE_F] = 0x2020202020202020;
+	mask_file_[FILE_G] = 0x4040404040404040;
+	mask_file_[FILE_H] = 0x8080808080808080;
 
 	const int BitTable[64] = { // precomputed table (https://www.chessprogramming.org/Looking_for_Magics)
 	63, 30, 3, 32, 25, 41, 22, 33, 15, 50, 42, 13, 11, 53, 19, 34, 61, 29, 2,
@@ -52,32 +52,27 @@ Board::Board() {
 
 	white_king_ = 1ULL << E1;
 	white_knights_ = 1ULL << B1 | 1ULL << G1;
-	white_pawns_ = 1ULL << A2 | 1ULL << B2 | 1ULL << C2 | 1ULL << D2 | 1ULL << E2 | 1ULL << F2 | 1ULL << G2 | 1ULL << H2 | 1ULL << G3 ;
-	black_pawns_ = 1ULL << A7 | 1ULL << B7 | 1ULL << C7 | 1ULL << D7 | 1ULL << E7 | 1ULL << F7 | 1ULL << G7 | 1ULL << H7 | 1ULL << H4;
+	white_pawns_ = 1ULL << A2 | 1ULL << B2 | 1ULL << C2 | 1ULL << D2 | 1ULL << E2 | 1ULL << F2 | 1ULL << G2 | 1ULL << H2 | 1ULL << G4;
+	black_pawns_ = 1ULL << A7 | 1ULL << B7 | 1ULL << C7 | 1ULL << D7 | 1ULL << E7 | 1ULL << F7 | 1ULL << G7 | 1ULL << H7 | 1ULL << H5 | 1ULL << F5;
 
-	for (int i = 0; i < 64; i++) {
+	for (int i = 0; i < 64; i++) { // assign board indexes to the piece array
 			piece_[i] = 1ULL << i;
-	};
-
-	white_king_moves_ = KingMoves(white_king_, white_pieces_);
-	black_king_moves_ = KingMoves(black_king_, black_pieces_);
-	white_knights_moves_ = KnightMoves(white_knights_, white_pieces_);
-	black_knights_moves_ = KnightMoves(black_knights_, black_pieces_);	
+	};	
 };
 
 void Board::ShowBoard(U64 bitboard) { // prints board to console
 	int rank = 0;
 	int file = 0;
-	int pos = 0;
+	int pos = 0; // number of times to shift left to assign position
 	std::cout << "\n";
 	for (rank = RANK_8; rank >= RANK_1; rank--) {
 		for (file = FILE_A; file <= FILE_H; file++) {
-			pos = rank * 8 + file;
+			pos = rank * 8 + file; // multiply rank by 8 to find correct row in bitboard, then add on file integer to move across
 			if ((bitboard & 1ULL << pos)) { // bitwise left shift to given position
-				std::cout << "1 ";
+				std::cout << "1 "; // bit is on; a piece is present
 			}
 			else {
-				std::cout << ". ";
+				std::cout << ". "; // bit is off; there is no piece at this position
 			}
 		}
 		std::cout << "\n";
@@ -151,22 +146,6 @@ U64 Board::GetAllPieces() {
 		return all_pieces_;
 };
 
-U64 Board::GetWhiteKingMoves() {
-	return white_king_moves_;
-};
-
-U64 Board::GetBlackKingMoves() {
-		return black_king_moves_;
-};
-
-U64 Board::GetWhiteKnightsMoves() {
-		return white_knights_moves_;
-};
-
-U64 Board::GetBlackKnightsMoves() {
-		return black_knights_moves_;
-};
-
 U64 Board::GetBitTable(int index) { // returns index from precomupted bit table
 		return bit_table[index];
 }
@@ -188,7 +167,7 @@ void Board::ClearBit(U64& bitboard, int index) { // removes a bit from the bitbo
 		bitboard &= ~GetPiece(index);
 }
 
-void Board::ResetBoard(Board b) {
+void Board::ResetBoard(Board b) { // reset board values to empty
 	int index = 0;
 	for (index = 0; index < 64; ++index) {
 		piece_array_[index] = piece::EMPTY;
@@ -254,8 +233,8 @@ U64 Board::WhitePawnMoves(U64 white_pawns, U64 black, U64 all) { // returns vali
 		// find position two spaces in front of pawn ONLY if the pawn is on the second rank
 		U64 white_pawn_double = white_pawns << 16 & GetMaskRank(RANK_4) & ~all; // the pawn can only move onto rank 4 if it started on rank 2
 		U64 white_pawn_moves = white_pawn_single | white_pawn_double;
-		U64 white_pawn_attack_left = white_pawns << 7 & GetClearFile(FILE_A); // clear file A 
-		U64 white_pawn_attack_right = white_pawns << 9 & GetClearFile(FILE_H);
+		U64 white_pawn_attack_left = white_pawns << 7 &GetClearFile(FILE_H); // clear file A 
+		U64 white_pawn_attack_right = white_pawns << 9 & GetClearFile(FILE_A);
 		U64 white_pawn_attacks = (white_pawn_attack_left | white_pawn_attack_right) & black; // AND with black pieces; attacking is only allowed when there is a target
 		U64 white_pawn_all = white_pawn_moves | white_pawn_attacks; // set of valid moves and attacks
 		return white_pawn_all;
@@ -265,8 +244,8 @@ U64 Board::BlackPawnMoves(U64 black_pawns, U64 black, U64 all) { // same as whit
 		U64 black_pawn_single = black_pawns >> 8 & ~all;
 		U64 black_pawn_double = black_pawns >> 16 & GetMaskRank(RANK_5) & ~all; 
 		U64 black_pawn_moves = black_pawn_single | black_pawn_double;
-		U64 black_pawn_attack_left = black_pawns >> 7 & GetClearFile(FILE_H);
-		U64 black_pawn_attack_right = black_pawns >> 9 & GetClearFile(FILE_A);
+		U64 black_pawn_attack_left = black_pawns >> 7 & GetClearFile(FILE_A);
+		U64 black_pawn_attack_right = black_pawns >> 9 & GetClearFile(FILE_H);
 		U64 black_pawn_attacks = (black_pawn_attack_left | black_pawn_attack_right) & black;
 		U64 black_pawn_all = black_pawn_moves | black_pawn_attacks;
 		return black_pawn_all;
